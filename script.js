52ab28cf17754f72ca626b46027d5f1358a4c5a9
jp200718 <amanda2020pinheiro@gmail.com>
2025-09-24 20:00:14 -0300

    Squashed commit of the following:
    commit ba050d4adc399212c54ebff4326cee6026337e01
    Author: jp200718 <amanda2020pinheiro@gmail.com>
    Date:   Mon Sep 22 20:03:04 2025 -0300
    
        mob next [ci-skip] [ci skip] [skip ci]
    
        lastFile:index.html

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
 const moon = document.getElementById("moon");
 const sun = document.getElementById("sun");
 const toggle = document.querySelector(".theme");
+const logos = document.querySelectorAll(".logo");
+const body = document.body;
+
 
 toggle.addEventListener("click", () => {
   moon.classList.toggle("show");
   sun.classList.toggle("show");
 
+  body.classList.toggle("dark");
+
   toggle.classList.toggle("sun")
+
+  logos.forEach(logo => {
+    logo.classList.toggle("show");
+  });
+});
+
+// Seleciona todos os botões
+const buttons = document.querySelectorAll(".filter-btn");
+
+buttons.forEach(btn => {
+  btn.addEventListener("click", () => {
+    // Remove a classe ativa de todos  tira a parte vermelha 'clicada'
+    buttons.forEach(b => b.classList.remove("active-btn"));
+    // Adiciona a parte vermelha no clicado
+    btn.classList.add("active-btn");
+  });
 });
