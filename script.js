fd06c5394b7f483010d98c10f665e0e0dbb9145e
Luiz <luizhc2309@hotmail.com>
2025-09-26 19:38:03 -0300

    Creating Function to track active and inactive states

--- script.js
+++ script.js
@@ -3,23 +3,56 @@ const sun = document.getElementById("sun");
 const toggle = document.querySelector(".theme");
 const logos = document.querySelectorAll(".logo");
 const body = document.body;
+const buttons = document.querySelectorAll(".filter-btn");
+const cards = document.querySelectorAll(".card");
+
+
+
+cards.forEach(card => {
+  const slider = card.querySelector(".input");
+
+
+  if (slider.checked) {
+    card.classList.add("active");
+    card.classList.remove("inactive");
+  } else {
+    card.classList.remove("active");
+    card.classList.add("inactive");
+  }
+
+  slider.addEventListener("change", (e) => {
+    if (e.target.checked) {
+      card.classList.add("active");
+      card.classList.remove("inactive");
+    } else {
+      card.classList.remove("active");
+      card.classList.add("inactive");
+    }
+  });
+  
+  
+});
+
+
 
 
 toggle.addEventListener("click", () => {
+  // Mostrar Elementos
   moon.classList.toggle("show");
   sun.classList.toggle("show");
-
-  body.classList.toggle("dark");
-
-  toggle.classList.toggle("sun")
-
   logos.forEach(logo => {
     logo.classList.toggle("show");
   });
-});
+  // Fim Mostrar Elementos
 
-// Seleciona todos os botões
-const buttons = document.querySelectorAll(".filter-btn");
+  // Mudar tema
+  body.classList.toggle("dark");
+  // Fim Mudar tema
+
+  // Animação
+  toggle.classList.toggle("sun")
+  // Fim Animação
+});
 
 buttons.forEach(btn => {
   btn.addEventListener("click", () => {
